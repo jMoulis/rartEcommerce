a563fb19204434ddc941cc74c5d64aea5271338b
julien Moulis <julien.moulis@moulis.me>
2024-10-24 10:32:48 +0200

    FixEnv

--- src/lib/stripe/stripe.ts
+++ src/lib/stripe/stripe.ts
@@ -2,6 +2,7 @@ import Stripe from 'stripe';
 
 const key = process.env.NODE_ENV === 'development' ? `${process.env.STRIPE_SECRET_KEY_TEST}` : `${process.env.STRIPE_SECRET_KEY_PROD}`;
 
+console.log('Stripe key:', key);
 export const stripe = new Stripe(key, {
   apiVersion: '2023-10-16',
   typescript: true,
